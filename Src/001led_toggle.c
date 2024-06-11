1ee0f84d455351a6537b99b97afa38663a863a7e
phamvantan <tanpham070@gmail.com>
2024-06-11 22:17:52 +0700

    test use git fork

--- Src/001led_toggle.c
+++ Src/001led_toggle.c
@@ -21,7 +21,7 @@ int main(void)
 	GPIO_Handle_t GpioLed;
 
 	GpioLed.pGPIOx = GPIOD;
-	GpioLed.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_12;
+	GpioLed.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_13;
 	GpioLed.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
 	GpioLed.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
 	GpioLed.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
